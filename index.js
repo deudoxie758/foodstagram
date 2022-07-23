0f81a266352192d97f72e1f1f175f0f3d4290460
Darnell Eudoxie <darnelleudoxie@gmail.com>
2022-07-23 12:45:21 -0400

    Posts Routes completed

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
 
 const app = express();
 const port = 3000;
 
 app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
