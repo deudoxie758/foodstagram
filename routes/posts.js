b2bc27fc1ade63e7aaad3b5146be7a15b7703544
Darnell Eudoxie <darnelleudoxie@gmail.com>
2022-07-24 12:34:11 -0400

    Completed error handling for post routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const _bodyParser = require("body-parser");
+const { PrismaClientKnownRequestError } = require("@prisma/client/runtime");
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -9,38 +10,77 @@ const bodyParser = _bodyParser.json();
 
 router.post("/", bodyParser, async (req, res) => {
   const data = req.body;
-  const new_post = await prisma.posts.create({ data });
-  res.json(new_post);
+  try {
+    const new_post = await prisma.posts.create({ data });
+    res.json(new_post);
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      console.log(err);
+    }
+    res.status(400).send("Error creating post");
+  }
 });
 
 router.get("/", async (req, res) => {
-  const all_posts = await prisma.posts.findMany();
-  res.json(all_posts);
+  try {
+    const all_posts = await prisma.posts.findMany();
+    res.json(all_posts);
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      console.log(err);
+    }
+    res.status(400).send("Error fetching posts");
+  }
 });
 
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const get_post = await prisma.posts.findUnique({
-    where: { id: parseInt(id) },
-  });
-  res.json(get_post);
+  try {
+    const { id } = req.params;
+    const get_post = await prisma.posts.findUnique({
+      where: { id: parseInt(id) },
+    });
+    if (!get_post) {
+      res.status(404).send("Post not found!");
+    } else {
+      res.json(get_post);
+    }
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      console.log(err);
+    }
+    res.status(400).send("Error fetching post");
+  }
 });
 
 router.put("/:id", bodyParser, async (req, res) => {
-  const { id } = req.params;
-  const data = req.body;
-  const updated_post = await prisma.posts.update({
-    where: { id: parseInt(id) },
-    data,
-  });
-  res.json(updated_post);
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const updated_post = await prisma.posts.update({
+      where: { id: parseInt(id) },
+      data,
+    });
+    res.json(updated_post);
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      console.log(err);
+    }
+    res.status(400).send("Error updating post");
+  }
 });
 
 router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  await prisma.posts.delete({
-    where: { id: parseInt(id) },
-  });
-  res.status(200).send("Deleted Successfully");
+  try {
+    const { id } = req.params;
+    await prisma.posts.delete({
+      where: { id: parseInt(id) },
+    });
+    res.status(200).send("Deleted Successfully");
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      console.log(err);
+    }
+    res.status(400).send("Error deleting post");
+  }
 });
 module.exports = router;
