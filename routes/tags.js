676e4005b6b2bf4b77e60c6f2cc856373f63ba87
Darnell Eudoxie <darnelleudoxie@gmail.com>
2022-07-24 13:27:35 -0400

    new primsa migration

--- routes/tags.js
+++ routes/tags.js
@@ -8,4 +8,17 @@ const prisma = new PrismaClient();
 
 const bodyParser = _bodyParser.json();
 
+router.post("/", async (req, res) => {
+  try {
+    const data = req.body;
+    const new_tag = await prisma.tags.create({ data });
+    res.json(new_tag);
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      console.log(err);
+    }
+    res.status(400).send("Error creating tag");
+  }
+});
+
 module.exports = router;
