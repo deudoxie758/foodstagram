1ec40e463d74ac1461a0ddea7cf8f26521d6b908
Darnell Eudoxie <darnelleudoxie@gmail.com>
2022-07-25 19:32:42 -0400

    add login route

--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,28 @@ const router = express.Router();
 const bodyParser = _bodyParser.json();
 const prisma = new PrismaClient();
 
-router.post("/login", bodyParser, async (req, res) => {});
+router.post("/login", bodyParser, async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const get_user = await prisma.users.findUnique({
+      where: { username },
+    });
+    if (get_user) {
+      const hash = get_user.hashed_password;
+      const is_authenticated = await bcrypt.compare(password, hash);
+      if (!is_authenticated) {
+        res.status(401).send("Unauthorized");
+      } else {
+        res.json(get_user);
+      }
+    } else {
+      res.status(401).send("Unauthorized: user not found");
+    }
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      console.log(err);
+    }
+    res.status(400).send("Error logging in");
+  }
+});
 module.exports = router;
