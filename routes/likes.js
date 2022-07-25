d8bc496ad1aca959353020c35a037b68abaecd1a
Darnell Eudoxie <darnelleudoxie@gmail.com>
2022-07-24 22:25:41 -0400

    likes routes completed

--- routes/likes.js
+++ routes/likes.js
@@ -2,9 +2,38 @@ const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const _bodyparser = require("body-parser");
 const { PrismaClientKnownRequestError } = require("@prisma/client/runtime");
+const e = require("express");
 
 const router = express.Router();
 const bodyParser = _bodyparser.json();
 const prisma = new PrismaClient();
 
+router.put("/:postID", bodyParser, async (req, res) => {
+  try {
+    const data = req.body;
+    const { postID } = req.params;
+    if (data.value === "like") {
+      const updated_post = await prisma.posts.update({
+        where: { id: parseInt(postID) },
+        data: {
+          likes: { increment: 1 },
+        },
+      });
+      res.json(updated_post);
+    } else {
+      const updated_post = await prisma.posts.update({
+        where: { id: parseInt(postID) },
+        data: {
+          likes: { decrement: 1 },
+        },
+      });
+      res.json(updated_post);
+    }
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      console.log(err);
+    }
+    res.status(400).send("Error updating likes");
+  }
+});
 module.exports = router;
